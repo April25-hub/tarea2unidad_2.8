5ce49d24f8b26ba92cb1beb68c0b6ebfb8fd9fe1
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-10-10 01:21:31 -0600

    Add files via upload

---
+++ main 8.py
@@ -0,0 +1,17 @@
+print(" ")
+print("Arzaba Diaz April 3W 1173")
+def mayor_de_tres(num1, num2, num3):
+    """
+    Esta funcion recibe tres numeros como argumentos y devuelve el mayor de ellos.
+    
+    Args:
+    num1 (float): El primer numero.
+    num2 (float): El segundo numero.
+    num3 (float): El tercer numero.
+    
+    Returns:
+    float: El mayor de los tres numeros.
+    """
+    return max(num1, num2, num3)
+
+  
